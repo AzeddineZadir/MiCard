28dea686c65f70f3d757679e635e8f903008978a
Azeddine Zadir <zadir.azeddine@gmail.com>
2021-09-29 01:13:04 +0200

    add colors and image to the avatar

---
+++ lib/Icons.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/widgets.dart';
+
+class MyFlutterApp {
+  MyFlutterApp._();
+
+  static const _kFontFam = 'MyFlutterApp';
+  static const String? _kFontPkg = null;
+
+  static const IconData mail_1 =
+      IconData(0xe801, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData github_alt =
+      IconData(0xf113, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData github =
+      IconData(0xf300, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData mark_github =
+      IconData(0xf308, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+import 'package:tp01_exo01/Icons.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: HexColor("#8EBFE1"),
         body: SafeArea(
             child: Center(
           child: Container(
@@ -24,10 +26,67 @@ class MyApp extends StatelessWidget {
                   decoration: BoxDecoration(
                       color: Colors.white,
                       border: Border.all(
-                        color: Colors.white70,
+                        color: HexColor("#8EBFE1"),
                       ),
                       borderRadius: BorderRadius.all(Radius.circular(20))),
-                  child: Column(),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        child: Text(
+                          'Azeddine Zadir',
+                          style: TextStyle(
+                            fontSize: 35.0,
+                            fontFamily: 'AlexBrush',
+                            fontWeight: FontWeight.bold,
+                            color: HexColor("#0A2463"),
+                          ),
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            margin: EdgeInsets.only(left: 6),
+                            child: Icon(
+                              Icons.email,
+                              size: 32.0,
+                              color: HexColor("#0A2463"),
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(left: 4.0),
+                            child: Text(
+                              'zadir.azeddine@gmail.com',
+                              style: TextStyle(
+                                  fontSize: 16.0, color: HexColor("#0A2463")),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            margin: EdgeInsets.only(left: 6),
+                            child: Icon(
+                              MyFlutterApp.github,
+                              size: 30.0,
+                              color: HexColor("#0A2463"),
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(left: 4.0),
+                            child: Text(
+                              'github.com/AzeddineZadir',
+                              style: TextStyle(
+                                  fontSize: 16.0, color: HexColor("#0A2463")),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
                 Positioned(
                   bottom: 290.0,
@@ -37,7 +96,8 @@ class MyApp extends StatelessWidget {
                     padding: EdgeInsets.only(bottom: 10),
                     decoration: BoxDecoration(
                       shape: BoxShape.circle,
-                      border: Border.all(color: Colors.teal, width: 4.0),
+                      border:
+                          Border.all(color: HexColor("#8EBFE1"), width: 4.0),
                       image: DecorationImage(
                         image: AssetImage('images/photo.jpg'),
                         fit: BoxFit.cover,
